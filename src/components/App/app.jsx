dcb2daa6a06d97297d74e8dae3f29e0d9339b9fb
Agito <gvagitoblgv@gmail.com>
2021-02-04 14:32:37 +0300

    eslint fix

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types';
 import MainPage from '../main-page/main-page.jsx';
 
 const App = ({title, genre, date}) => {
-    return (
-        <MainPage
-           title={title}
-           genre={genre}
-           date={date}
-        />
-    );
+  return (
+    <MainPage
+      title={title}
+      genre={genre}
+      date={date}
+    />
+  );
 };
 
 App.propTypes = {
-    title: PropTypes.string.isRequired,
-    genre: PropTypes.string.isRequired,
-    date: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  date: PropTypes.number.isRequired,
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './components/App/app.jsx';
 
 const data = {
-    title: `The Grand Budapest Hotel`,
-    genre: `Drama`,
-    date: 2014,
+  title: `The Grand Budapest Hotel`,
+  genre: `Drama`,
+  date: 2014,
 };
 
 ReactDOM.render(
-    <App 
-        title={data.title}
-        genre={data.genre}
-        date={data.date}
+    <App
+      title={data.title}
+      genre={data.genre}
+      date={data.date}
     />,
     document.querySelector(`#root`)
 );
