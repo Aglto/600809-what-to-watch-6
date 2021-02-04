fa088cea3aa9d4475c3de008325746d5e0a014ab
Agito <gvagitoblgv@gmail.com>
2021-02-04 14:25:01 +0300

    We start programming

---
+++ src/components/App/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MainPage from '../main-page/main-page.jsx';
+
+const App = ({title, genre, date}) => {
+    return (
+        <MainPage
+           title={title}
+           genre={genre}
+           date={date}
+        />
+    );
+};
+
+App.propTypes = {
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    date: PropTypes.number.isRequired,
+};
+
+export default App;
