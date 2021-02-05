b56f02f6e6f9ae90c3853d5ab3a177204b3b4a26
Agito <gvagitoblgv@gmail.com>
2021-02-05 13:23:39 +0300

    Added a component for the login page

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MainPage from '../main-page/main-page.jsx';
+import SignIn from '../sign-in/sign-in';
 
 const App = ({title, genre, date}) => {
   return (
