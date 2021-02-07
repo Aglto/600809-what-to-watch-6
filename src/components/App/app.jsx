9f8fc8d7fe2cf86a83add5deacde98eac3454380
Agito <gvagitoblgv@gmail.com>
2021-02-07 16:42:20 +0300

    module2-task1

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,31 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import MainPage from '../main-page/main-page';
 import SignIn from '../sign-in/sign-in';
 import MyList from '../my-list/my-list';
 import MoviePageReviews from '../movie-page-reviews/movie-page-reviews';
 import Player from '../player/player';
 import FilmPage from '../film-page/film-page';
+import NotFoundPage from '../not-found-page/not-found-page';
 
-const App = ({title, genre, date}) => {
+const App = (props) => {
+
+  const {title, genre, date} = props;
+
   return (
-    
+
     <BrowserRouter>
       <Switch>
 
-        <Route>
-        <MainPage
-          title={title}
-          genre={genre}
-          date={date}
-        />
+        <Route path='/' exact>
+          <MainPage
+            title={title}
+            genre={genre}
+            date={date}
+          />
+        </Route>
+
+        <Route path='/login' exact>
+          <SignIn />
+        </Route>
+
+        <Route path='/mylist' exact>
+          <MyList />
+        </Route>
+
+        <Route path='/films/:id' exact>
+          <FilmPage />
+        </Route>
+
+        <Route path='/films/:id/review' exact>
+          <MoviePageReviews />
+        </Route>
+
+        <Route path='/player/:id' exact>
+          <Player />
         </Route>
 
         <Route>
-
+          <NotFoundPage />
         </Route>
-        
+
       </Switch>
     </BrowserRouter>
   );
--- src/components/not-found-page/not-found-page.jsx
+++ src/components/not-found-page/not-found-page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
-const SignIn = () => {
+const NotFoundPage = () => {
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
@@ -11,28 +12,11 @@ const SignIn = () => {
             <span className="logo__letter logo__letter--3">W</span>
           </a>
         </div>
-
-        <h1 className="page-title user-page__title">Sign in</h1>
       </header>
-
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
-          <div className="sign-in__fields">
-            <div className="sign-in__field">
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
-              <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
-            </div>
-            <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
-              <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
-            </div>
-          </div>
-          <div className="sign-in__submit">
-            <button className="sign-in__btn" type="submit">Sign in</button>
-          </div>
-        </form>
+        <h1>404 Not Found</h1>
+        <NavLink to="/">Вернуться на главную страницу</NavLink>
       </div>
-
       <footer className="page-footer">
         <div className="logo">
           <a href="main.html" className="logo__link logo__link--light">
@@ -41,7 +25,6 @@ const SignIn = () => {
             <span className="logo__letter logo__letter--3">W</span>
           </a>
         </div>
-
         <div className="copyright">
           <p>© 2019 What to watch Ltd.</p>
         </div>
@@ -50,4 +33,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default NotFoundPage;
